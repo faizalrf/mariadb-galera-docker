15cdf8e45a64b6be0c34bcce6c0567a67692dc70
Faisal Saeed <faisal@mariadb.com>
2021-09-20 03:30:26 +0800

    I Refactori you 3000

--- init/01.sql
+++ init/01.sql
@@ -3,3 +3,6 @@ GRANT ALL ON *.* TO mxs@'%';
 
 CREATE USER app_user@'%' IDENTIFIED BY 'password';
 GRANT ALL ON testdb.* to app_user@'%';
+
+CREATE USER mariabackup@localhost identified by 'password';
+GRANT RELOAD, PROCESS, LOCK TABLES, REPLICATION CLIENT, REPLICATION SLAVE ADMIN ON *.* TO mariabackup@localhost;
