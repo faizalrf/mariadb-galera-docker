1b8d1349cf387dc5a09346ac28ee522178396100
Faisal Saeed <faisal@mariadb.com>
2021-09-23 03:12:39 +0800

    Update RAM

--- test-scripts/dc.sh
+++ test-scripts/dc.sh
@@ -3,8 +3,8 @@
 TableName=tab1
 UserName=app_user
 PassWord=password
-maxHost=172.20.0.5
-Port=500
+maxHost=127.0.0.1
+Port=5001
 
 mariadb -u ${UserName} -p${PassWord} -h${maxHost} -P${Port} -e "DROP DATABASE IF EXISTS testdb; CREATE DATABASE IF NOT EXISTS testdb; CREATE TABLE IF NOT EXISTS testdb.${TableName} (id serial, c1 varchar(100), ts timestamp(6));"
 
@@ -16,6 +16,6 @@ do
     echo -e "SELECT concat('SELECT FROM ${TableName} on ', @@hostname, ' ->'), rpad($i, 10, '.'), 
                 IF(COUNT(*)> 0, '\033[0;32mRecord Found\033[0m','\033[0;31m! Not Found !\033[0m' ) 
             FROM testdb.${TableName} WHERE id = $i;"
-    i=$((i+1))
-    sleep 0.05
+    i=$((i+6))
+    sleep 0.03
 done | mariadb -N -u ${UserName} -p${PassWord} -h${maxHost} -P${Port}
