a9efb7baa65eec03a76d30509b2d8ed910c7d589
Faisal Saeed <faisal@mariadb.com>
2021-09-27 00:24:09 +0800

    Bootstrap settled

--- galera/startup.sh
+++ galera/startup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# TODO Add logic for bootstrap! 
 StartUp="/usr/local/bin/docker-entrypoint.sh mysqld"
 
 # If BOOTSTRAP argument is passed as 1 then start the node as galera_new_cluster
 if [ ${BOOTSTRAP} -eq 1 ]; then
-   StartUp="${StartUp} --wsrep-new-cluster"
-   sed -i -e 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/g' /var/lib/mysql/grastate.dat
-else
-   sed -i -e 's/safe_to_bootstrap: 1/safe_to_bootstrap: 0/g' /var/lib/mysql/grastate.dat
+   if [ -f /var/lib/mysql/bootstrap.ind ]; then
+      StartUp="${StartUp} --wsrep-new-cluster"
+   fi
 fi
+
+rm -rf /var/lib/mysql/bootstrap.ind
 
 exec ${StartUp}
